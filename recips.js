fc4a5c472d24217c42743ceffcc747dc4e767b68
joseetravaux-cloud <joseetravaux@gmail.com>
2025-10-31 23:18:55 -0400

    Create recips.js

---
+++ recips.js
@@ -0,0 +1,55 @@
+[
+  {
+    "titre": "Muffins moelleux au Thermomix",
+    "page": "recette1.html",
+    "image": "https://picsum.photos/300/200?random=1"
+  },
+  {
+    "titre": "Soupe veloutée de courge",
+    "page": "recette2.html",
+    "image": "https://picsum.photos/300/200?random=2"
+  },
+  {
+    "titre": "Pain maison facile",
+    "page": "recette3.html",
+    "image": "https://picsum.photos/300/200?random=3"
+  },
+  {
+    "titre": "Smoothie tropical",
+    "page": "recette4.html",
+    "image": "https://picsum.photos/300/200?random=4"
+  },
+  {
+    "titre": "Pâte à pizza express",
+    "page": "recette5.html",
+    "image": "https://picsum.photos/300/200?random=5"
+  },
+  {
+    "titre": "Crème dessert au chocolat",
+    "page": "recette6.html",
+    "image": "https://picsum.photos/300/200?random=6"
+  },
+  {
+    "titre": "Compote de pommes maison",
+    "page": "recette7.html",
+    "image": "https://picsum.photos/300/200?random=7"
+  },
+  {
+    "titre": "Risotto aux champignons",
+    "page": "recette8.html",
+    "image": "https://picsum.photos/300/200?random=8"
+  },
+  {
+    "titre": "Pesto vert rapide",
+    "page": "recette9.html",
+    "image": "https://picsum.photos/300/200?random=9"
+  },
+  {
+    "titre": "Biscuits sablés au beurre",
+    "page": "recette10.html",
+    "image": "https://picsum.photos/300/200?random=10"
+  }
+]
+
+
+
